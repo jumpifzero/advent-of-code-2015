8ebb36cd54a9e9a6e62ec9ddb8f48583d406873c
Tiago Almeida <tiago.b.almeida@gmail.com>
2015-12-24 13:40:12 +0000

    Nicer comments

--- 24/main.py
+++ 24/main.py
@@ -1,3 +1,7 @@
+# Advent of code day 24 part 1 challenge
+# http://adventofcode.com/day/24
+# Tiago Almeida, 2015
+
 
 items = [1,2,3,5,7,13,17,19,23,29,31,37,
 		41,43,53,59,61,67,71,73,79,83,89,
@@ -7,12 +11,13 @@ items = [1,2,3,5,7,13,17,19,23,29,31,37,
 def part1():
 	"""
 	Solution approach:
-	 - get compartment weight (cw) for each compartment (cw)
-	 - get combinations of N items that match cw for all N from 2
-	 	to N. Stop when you find at least one combination of items
+	 - calculate compartment weight (cw) for each compartment (cw)
+	 - generate combinations of N items for all N from 1
+	 	to N that have correct weight. 
+	 	Stop when you find at least one combination of items
 	 	as this will give at least one combination with minimum 
-	 	number of items and correct weight. Then, minimize their
-	 	entanglement.
+	 	number of items and correct weight. 
+	 - Mnimize the entanglement and return the value
 	"""
 	# get compartment weight (cw) for each compartment
 	cw = sum(items) / 3
@@ -20,9 +25,9 @@ def part1():
 	# start with 2 items, then increase number of items until 1 or more 
 	# combinations with weight cw are found
 	from itertools import combinations
-	for n in range(2, len(items)+1):
+	for n in range(1, len(items)+1):
 		combos = [ x for x in combinations(items,n) if sum(x) == cw ] 
-		# combos -> combinations with that weight
+		# combos -> combinations of n items with weight cw
 		if combos:
 			break
 	# minimize entanglement
